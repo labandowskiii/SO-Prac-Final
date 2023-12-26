71f8b92d880b4408f7ed258bc0f9af382e84b651
dcontt00 <dcontt00@estudiantes.unileon.es>
2023-12-26 16:23:40 +0100

    Añadidas variables globales

--- practicafinal.c
+++ practicafinal.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+/*STRCUTS*/
+struct  Cliente{
+    int clienteId;
+    int estado; // 0: Esperando | 1: En cajero | 2: atendido
+};
+
+/*VARIABLES GLOBALES */
+char rutaArchivoLog[20]="programa.log";
+FILE *fichero;
+// Lista de clientes
+struct Cliente *clientes;
+pthread_mutex_t mutexLog;
+pthread_mutex_t mutexListaClientes;
+pthread_cond_t condicionInteractuarReponedor;
+pthread_mutex_t mutexInteractuarReponedor;
+
+
+
 int main(int argc, char *argv[]){
     return 0;
 }
